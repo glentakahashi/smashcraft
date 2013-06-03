d612f3c5c36b64dd01b90b7f445d79dfaa014232
Steven La <mrstevenla@gmail.com>
2013-06-02 23:43:11 -0700

    Increased range slightly

--- constants.js
+++ constants.js
@@ -13,7 +13,7 @@ function charClone(name, id) {
     launchResistance: 500.0,
     attacks: {
       neutral: {
-        range: vec3.fromValues(100.0, 3.5, 4.5),
+        range: vec3.fromValues(100.0, 5.5, 6.5),
         facing: true,
         knockback: {
           angle: Math.PI / 6,
@@ -30,7 +30,7 @@ function charClone(name, id) {
         sound: 'punchHit',
       },
       sideSmash: {
-        range: vec3.fromValues(100.0, 3.5, 4.5),
+        range: vec3.fromValues(100.0, 6.5, 8.5),
         facing: true,
         knockback: {
           angle: Math.PI / 3.5,
