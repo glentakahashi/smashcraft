d71559d34cda96a28766c10581ad6607e4e403d6
Alexander Ramirez <adr2370@gmail.com>
2013-06-03 22:53:23 -0700

    Updated terrain

--- game.js
+++ game.js
@@ -13,13 +13,13 @@ function Game(stageNum,players,p1,p2,p3,p4) {
 	case 5:
 		//grassland
 		self.platforms.push(new Platform(vec3.fromValues(8.0, 32.0, 30.0), vec3.fromValues(0.0, -32.0, 0.0), "Block", "grass"));
-		self.platforms.push(new Platform(vec3.fromValues(8.0, 1.0, 6.0), vec3.fromValues(0.0, 12.0, -14.0), "Trans", "wood"));
+		self.platforms.push(new Platform(vec3.fromValues(8.0, 1.0, 6.0), vec3.fromValues(0.0, 12.0, -14.0), "Trans", "dirt"));
 		self.platforms.push(new Platform(vec3.fromValues(8.0, 1.0, 6.0), vec3.fromValues(0.0, 12.0, 14.0), "Trans", "dirt"));
-		self.platforms.push(new Platform(vec3.fromValues(8.0, 1.0, 6.0), vec3.fromValues(0.0, 24.0, 0.0), "Trans", "stone"));
+		self.platforms.push(new Platform(vec3.fromValues(8.0, 1.0, 6.0), vec3.fromValues(0.0, 24.0, 0.0), "Trans", "dirt"));
 		break;
 	case 6:
 		//jungle
-		self.platforms.push(new Platform(vec3.fromValues(8.0, 2.0, 30.0), vec3.fromValues(0.0, 0.0, 0.0), "Block"));
+		self.platforms.push(new Platform(vec3.fromValues(8.0, 2.0, 30.0), vec3.fromValues(0.0, 0.0, 0.0), "Block", "dirt"));
 		self.platforms.push(new Platform(vec3.fromValues(6.0, 1.0, 6.0), vec3.fromValues(0.0, 12.0, -14.0), "Trans", "wood"));
 		self.platforms.push(new Platform(vec3.fromValues(6.0, 1.0, 6.0), vec3.fromValues(0.0, 12.0, 14.0), "Trans", "wood"));
 		self.platforms.push(new Platform(vec3.fromValues(6.0, 1.0, 6.0), vec3.fromValues(0.0, 24.0, -21.0), "Trans", "wood"));
@@ -59,9 +59,9 @@ function Game(stageNum,players,p1,p2,p3,p4) {
 		break;
 	case 0:
 		//ocean
-		self.platforms.push(new Platform(vec3.fromValues(8.0, 2.0, 10.0), vec3.fromValues(0.0, 0.0, -25.0), "Block"));
-		self.platforms.push(new Platform(vec3.fromValues(8.0, 2.0, 10.0), vec3.fromValues(0.0, 0.0, 0.0), "Block"));
-		self.platforms.push(new Platform(vec3.fromValues(8.0, 2.0, 10.0), vec3.fromValues(0.0, 0.0, 25.0), "Block"));
+		self.platforms.push(new Platform(vec3.fromValues(8.0, 2.0, 10.0), vec3.fromValues(0.0, 0.0, -25.0), "Block", "dirt"));
+		self.platforms.push(new Platform(vec3.fromValues(8.0, 2.0, 10.0), vec3.fromValues(0.0, 0.0, 0.0), "Block", "dirt"));
+		self.platforms.push(new Platform(vec3.fromValues(8.0, 2.0, 10.0), vec3.fromValues(0.0, 0.0, 25.0), "Block", "dirt"));
 		break;
   }
 
